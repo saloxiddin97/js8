0ac3333b267ef7188be752b884fae64a9a88a25d
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-06-13 13:26:40 +0500

    gggfd

---
+++ script.js
@@ -0,0 +1,18 @@
+let numbers=[1, 2 ,3, 4, 5, , 7, 8, 9,10 ] 
+let pustoy=[];
+for(i = 0; i < numbers.length; i++){
+ if(numbers[i] % 2 == 0) {
+   pustoy.push(numbers[i]);
+}
+}
+console.log(pustoy);
+
+
+ let number=[1, 2 ,3, 4, 5, 6, 7, 8, 9,10 ] 
+ let pusto=[];
+ for(i = 0; i < number.length; i++){
+  if(number[i] % 3 == 0) {
+    pusto.push(number[i]);
+ }
+ }
+ console.log(pusto);
